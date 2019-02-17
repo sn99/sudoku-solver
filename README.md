6a646c8ca92c17eb9f655f2872642021e919f5de
sn99 <siddharthn.099@gmail.com>
2019-02-18 04:54:34 +0530

    edited readme, improved speed

--- README.md
+++ README.md
@@ -2,4 +2,35 @@
 
 [![Build Status](https://travis-ci.com/sn99/sudoku-solver.svg?branch=master)](https://travis-ci.com/sn99/sudoku-solver)
 
-A sudoku solver
+A sudoku solver in Rust
+
+Usage : `cargo run --release`
+
+Use `0` to show blank spaces in the grid
+
+Eg - 
+```
+0 0 8 0 0 0 9 0 0
+3 5 0 0 0 0 0 8 6
+0 7 0 9 0 6 0 3 0
+8 0 2 0 9 0 6 0 1
+0 0 0 7 0 8 0 0 0
+7 0 5 0 2 0 4 0 8
+0 2 0 1 0 3 0 6 0
+9 6 0 0 0 0 0 5 2
+0 0 3 0 0 0 7 0 0
+ ```
+gives
+```
+6 1 8  5 3 2  9 4 7 
+3 5 9  4 1 7  2 8 6 
+2 7 4  9 8 6  1 3 5 
+
+8 4 2  3 9 5  6 7 1 
+1 9 6  7 4 8  5 2 3 
+7 3 5  6 2 1  4 9 8 
+
+4 2 7  1 5 3  8 6 9 
+9 6 1  8 7 4  3 5 2 
+5 8 3  2 6 9  7 1 4
+```
--- src/main.rs
+++ src/main.rs
@@ -1,258 +1,254 @@
 #[derive(Debug, Clone)]
 struct Cell {
-	value: usize,
-	possibilities: Option<Vec<usize>>,
+    value: usize,
+    possibilities: Option<Vec<usize>>,
 }
 
 impl Cell {
-	fn new(value: usize) -> Self {
-		let possibilities = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
-		match value {
-			0 => {
-				return Cell {
-					value,
-					possibilities: Some(possibilities),
-				};
-			}
+    fn new(value: usize) -> Self {
+        let possibilities = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+        match value {
+            0 => Cell {
+                value,
+                possibilities: Some(possibilities),
+            },
 
-			_ => {
-				return Cell {
-					value,
-					possibilities: None,
-				};
-			}
-		}
-	}
+            _ => Cell {
+                value,
+                possibilities: None,
+            },
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
 struct Grid {
-	grid: Vec<Vec<Cell>>,
-	mutated_state: bool,
+    grid: Vec<Vec<Cell>>,
+    mutated_state: bool,
 }
 
 impl Grid {
-	fn new() -> Self {
-		let mut grid = Vec::with_capacity(9);
-		let mutated_state = false;
+    fn new() -> Self {
+        let mut grid = Vec::with_capacity(9);
+        let mutated_state = false;
 
-		let k = Grid::input_sudoku();
+        let k = Grid::input_sudoku();
 
-		for q in k {
-			let mut temp_grid_line = Vec::with_capacity(9);
-			for w in q {
-				temp_grid_line.push(Cell::new(w));
-			}
-			grid.push(temp_grid_line);
-		}
+        for q in k {
+            let mut temp_grid_line = Vec::with_capacity(9);
+            for w in q {
+                temp_grid_line.push(Cell::new(w));
+            }
+            grid.push(temp_grid_line);
+        }
 
-		Grid {
-			grid,
-			mutated_state,
-		}
-	}
+        Grid {
+            grid,
+            mutated_state,
+        }
+    }
 
-	fn input_sudoku() -> Vec<Vec<usize>> {
-		let mut vec1 = Vec::with_capacity(9);
+    fn input_sudoku() -> Vec<Vec<usize>> {
+        let mut vec1 = Vec::with_capacity(9);
 
-		for _ in 0..9 {
-			let mut input_text = String::new();
-			std::io::stdin()
-				.read_line(&mut input_text)
-				.expect("Failed to read line");
+        for _ in 0..9 {
+            let mut input_text = String::new();
+            std::io::stdin()
+                .read_line(&mut input_text)
+                .expect("Failed to read line");
 
-			let vec2: Vec<usize> = input_text
-				.trim()
-				.split(' ')
-				.flat_map(str::parse::<usize>)
-				.collect::<Vec<_>>();
-			vec1.push(vec2);
-		}
+            let vec2: Vec<usize> = input_text
+                .trim()
+                .split(' ')
+                .flat_map(str::parse::<usize>)
+                .collect::<Vec<_>>();
+            vec1.push(vec2);
+        }
 
-		vec1
-	}
+        vec1
+    }
 
-	fn show(&self) {
-		let mut k = 0;
-		let mut j = 0;
-		for x in &self.grid {
-			for j in x {
-				if k == 3 || k == 6 {
-					print!(" ");
-				}
-				print!("{} ", j.value);
-				k += 1;
-			}
-			println!();
-			if j == 2 || j == 5 {
-				println!();
-			}
-			j += 1;
-			k = 0;
-		}
-	}
+    fn show(&self) {
+        let mut k = 0;
+        for (j, x) in self.grid.iter().enumerate() {
+            for j in x {
+                if k == 3 || k == 6 {
+                    print!(" ");
+                }
+                print!("{} ", j.value);
+                k += 1;
+            }
+            println!();
+            if j == 2 || j == 5 {
+                println!();
+            }
+            k = 0;
+        }
+    }
 
-	fn solve(&mut self) {
-		loop {
-			self.remove_possibilities_row();
-			self.remove_possibilities_column();
-			self.remove_possibilities_box();
-			self.change_value_one_possibility();
-			self.change_value_others_possibility();
+    fn solve(&mut self) {
+        loop {
+            self.remove_possibilities_row();
+            self.remove_possibilities_column();
+            self.remove_possibilities_box();
+            self.change_value_one_possibility();
+            self.change_value_others_possibility();
 
-			if !self.mutated_state {
-				break;
-			}
+            if !self.mutated_state {
+                break;
+            }
 
-			self.mutated_state = false;
-		}
-	}
+            self.mutated_state = false;
+        }
+    }
 
-	fn change_value_one_possibility(&mut self) {
-		for row in &mut self.grid {
-			for column in row {
-				match column.possibilities {
-					Some(ref possibilities) => {
-						if possibilities.len() == 1 {
-							column.value = possibilities.clone().pop().unwrap();
-							column.possibilities = None;
-							self.mutated_state = true;
-						}
-					}
-					None => continue,
-				}
-			}
-		}
-	}
+    fn change_value_one_possibility(&mut self) {
+        for row in &mut self.grid {
+            for column in row {
+                match column.possibilities {
+                    Some(ref possibilities) => {
+                        if possibilities.len() == 1 {
+                            column.value = possibilities.clone().pop().unwrap();
+                            column.possibilities = None;
+                            self.mutated_state = true;
+                        }
+                    }
+                    None => continue,
+                }
+            }
+        }
+    }
 
-	fn change_value_others_possibility(&mut self) {
-		let mut column_jump = 0;
-		let mut row_jump = 0;
-		for _ in 0..3 {
-			for _ in 0..3 {
-				let mut q = Vec::new();
-				for row in 0..3 {
-					for column in 0..3 {
-						match &self.grid[row + row_jump][column + column_jump].possibilities.clone() {
-							Some(possibilities) => {
-								for k in possibilities {
-									q.push(k.clone());
-								}
-							}
-							None => ()
-						}
-					}
-				}
+    fn change_value_others_possibility(&mut self) {
+        let mut column_jump = 0;
+        let mut row_jump = 0;
+        for _ in 0..3 {
+            for _ in 0..3 {
+                let mut q = Vec::new();
+                for row in 0..3 {
+                    for column in 0..3 {
+                        match &self.grid[row + row_jump][column + column_jump]
+                            .possibilities
+                            .clone()
+                        {
+                            Some(possibilities) => {
+                                for k in possibilities {
+                                    q.push(k.clone());
+                                }
+                            }
+                            None => (),
+                        }
+                    }
+                }
 
-				q.sort();
-				q.dedup();
+                q.sort();
+                q.dedup();
 
-				for row in 0..3 {
-					for column in 0..3 {
-						match &self.grid[row + row_jump][column + column_jump].possibilities.clone() {
-							Some(possibilities) => {
-								for k in possibilities {
-									match q.iter().find(|&&x| x == *k) {
-										None => {
-											self.grid[row + row_jump][column + column_jump].value = *k;
-											self.grid[row + row_jump][column + column_jump].possibilities = None;
-											self.mutated_state = true;
-										}
-										_ => ()
-									}
-								}
-							}
-							_ => ()
-						}
-					}
-				}
-				column_jump += 3;
-			}
-			row_jump += 3;
-			column_jump = 0;
-		}
-	}
+                for row in 0..3 {
+                    for column in 0..3 {
+                        if let Some(possibilities) = &self.grid[row + row_jump]
+                            [column + column_jump]
+                            .possibilities
+                            .clone()
+                        {
+                            for k in possibilities {
+                                if q.iter().find(|&&x| x == *k).is_none() {
+                                    self.grid[row + row_jump][column + column_jump].value = *k;
+                                    self.grid[row + row_jump][column + column_jump].possibilities =
+                                        None;
+                                    self.mutated_state = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                column_jump += 3;
+            }
+            row_jump += 3;
+            column_jump = 0;
+        }
+    }
 
-	fn remove_possibilities_box(&mut self) {
-		let zx = self.grid.clone();
-		let mut column_jump = 0;
-		let mut row_jump = 0;
-		for _ in 0..3 {
-			for _ in 0..3 {
-				for row in 0..3 {
-					for column in 0..3 {
-						match &mut self.grid[row + row_jump][column + column_jump].possibilities {
-							Some(possibilities) => {
-								for x in 0..3 {
-									for y in 0..3 {
-										let q = zx[x + row_jump][y + column_jump].value;
-										if x != row + row_jump && y != column + column_jump {
-											possibilities.retain(|x| *x != q);
-										}
-									}
-								}
-							}
-							None => continue,
-						}
-					}
-				}
-				column_jump += 3;
-			}
-			row_jump += 3;
-			column_jump = 0;
-		}
-	}
+    fn remove_possibilities_box(&mut self) {
+        let zx = self.grid.clone();
+        let mut column_jump = 0;
+        let mut row_jump = 0;
+        for _ in 0..3 {
+            for _ in 0..3 {
+                for row in 0..3 {
+                    for column in 0..3 {
+                        match &mut self.grid[row + row_jump][column + column_jump].possibilities {
+                            Some(possibilities) => {
+                                for x in 0..3 {
+                                    for y in 0..3 {
+                                        let q = zx[x + row_jump][y + column_jump].value;
+                                        if x != row + row_jump && y != column + column_jump {
+                                            possibilities.retain(|x| *x != q);
+                                        }
+                                    }
+                                }
+                            }
+                            None => continue,
+                        }
+                    }
+                }
+                column_jump += 3;
+            }
+            row_jump += 3;
+            column_jump = 0;
+        }
+    }
 
-	fn remove_possibilities_column(&mut self) {
-		let zx = self.grid.clone();
-		for row in 0..9 {
-			for column in 0..9 {
-				match &mut self.grid[row][column].possibilities {
-					Some(possibilities) => {
-						for i in 0..9 {
-							if i != row {
-								let q = zx[i][column].value;
-								possibilities.retain(|x| *x != q);
-							}
-						}
-					}
-					None => continue,
-				}
-			}
-		}
-	}
+    fn remove_possibilities_column(&mut self) {
+        let zx = self.grid.clone();
+        for row in 0..9 {
+            for column in 0..9 {
+                match &mut self.grid[row][column].possibilities {
+                    Some(possibilities) => {
+                        for (i, q) in zx.iter().enumerate().take(9) {
+                            if i != row {
+                                let q = q[column].value;
+                                possibilities.retain(|x| *x != q);
+                            }
+                        }
+                    }
+                    None => continue,
+                }
+            }
+        }
+    }
 
-	fn remove_possibilities_row(&mut self) {
-		for row in 0..9 {
-			for column in 0..9 {
-				let zx = self.grid.clone();
+    fn remove_possibilities_row(&mut self) {
+        for row in 0..9 {
+            for column in 0..9 {
+                let zx = self.grid.clone();
 
-				match &mut self.grid[row][column].possibilities {
-					Some(possibilities) => {
-						for i in 0..9 {
-							if i != column {
-								let q = zx[row][i].value;
-								possibilities.retain(|x| *x != q);
-							}
-						}
-					}
-					None => continue,
-				}
-			}
-		}
-	}
+                match &mut self.grid[row][column].possibilities {
+                    Some(possibilities) => {
+                        for i in 0..9 {
+                            if i != column {
+                                let q = zx[row][i].value;
+                                possibilities.retain(|x| *x != q);
+                            }
+                        }
+                    }
+                    None => continue,
+                }
+            }
+        }
+    }
 }
 
 fn main() {
-	let mut k = Grid::new();
+    let mut k = Grid::new();
 
-	println!("{:?}\n", k);
+    println!("{:?}\n", k);
 
-	k.solve();
+    k.solve();
 
-	println!("\n{:#?}", k);
+    println!("\n{:#?}", k);
 
-	k.show();
+    k.show();
 }
 
 /*
